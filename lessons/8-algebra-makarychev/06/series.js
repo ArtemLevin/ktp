2d7b4b82522618ea8a665c9eb6cc83d67b622de6
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-04 15:25:59 +0300

    content: scaffold grade 8 integer powers series

---
+++ lessons/8-algebra-makarychev/06/series.js
@@ -0,0 +1,25 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={
+  meta:{
+    rowId:'8-algebra-makarychev',
+    topicIndex:5,
+    topicId:'8-algebra-makarychev::5',
+    topicNumber:6,
+    grade:8,
+    subject:'Алгебра',
+    book:'Макарычев',
+    topicTitle:'Степень с целым показателем',
+    schoolYear:'2026/27',
+    plannedWeeks:'28–30',
+    totalLessons:8,
+    courseLessonStart:83,
+    courseLessonEnd:90,
+    topicHref:'../../../topics/8-algebra-makarychev/06.html',
+    ktpHref:'../../../index.html?focus=8-algebra-makarychev&view=timeline',
+    catalogHref:'index.html'
+  },
+  corrections:{},
+  lessons:[]
+};
+})();
