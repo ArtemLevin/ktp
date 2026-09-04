ec94d6652b0cf0e765f7be9ff099289925a57c90
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-04 09:03:30 +0300

    feat: start grade 7 final review lesson series

---
+++ lessons/7-algebra-makarychev/07/series.js
@@ -0,0 +1,7 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={
+  meta:{rowId:'7-algebra-makarychev',topicIndex:6,topicId:'7-algebra-makarychev::6',topicNumber:7,grade:7,subject:'Алгебра',book:'Макарычев',topicTitle:'Повторение (12 ч). Уроки 91–102',schoolYear:'2026/27',plannedWeeks:'31–34',totalLessons:12,topicHref:'../../../topics/7-algebra-makarychev/07.html',ktpHref:'../../../index.html?focus=7-algebra-makarychev&view=timeline',catalogHref:'index.html'},
+  corrections:{},lessons:[]
+};
+})();
