da89a7ef441095e4a161e79c9306e51dca3782e2
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-04 15:47:04 +0300

    docs: scaffold grade 9 Makarychev lesson plan

---
+++ lessons/9-algebra-makarychev/02/series.js
@@ -0,0 +1,25 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={
+  meta:{
+    rowId:'9-algebra-makarychev',
+    topicIndex:1,
+    topicId:'9-algebra-makarychev::1',
+    topicNumber:2,
+    grade:9,
+    subject:'Алгебра',
+    book:'Макарычев',
+    topicTitle:'Функции и графики',
+    schoolYear:'2026/27',
+    plannedWeeks:'7–12',
+    totalLessons:16,
+    courseLessonStart:19,
+    courseLessonEnd:34,
+    topicHref:'../../../topics/9-algebra-makarychev/02.html',
+    ktpHref:'../../../index.html?focus=9-algebra-makarychev&view=timeline',
+    catalogHref:'index.html'
+  },
+  corrections:{},
+  lessons:[]
+};
+})();
---
+++ lessons/9-algebra-makarychev/04/series.js
@@ -0,0 +1,25 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={
+  meta:{
+    rowId:'9-algebra-makarychev',
+    topicIndex:3,
+    topicId:'9-algebra-makarychev::3',
+    topicNumber:4,
+    grade:9,
+    subject:'Алгебра',
+    book:'Макарычев',
+    topicTitle:'Уравнения и неравенства с двумя переменными',
+    schoolYear:'2026/27',
+    plannedWeeks:'19–25',
+    totalLessons:20,
+    courseLessonStart:55,
+    courseLessonEnd:74,
+    topicHref:'../../../topics/9-algebra-makarychev/04.html',
+    ktpHref:'../../../index.html?focus=9-algebra-makarychev&view=timeline',
+    catalogHref:'index.html'
+  },
+  corrections:{},
+  lessons:[]
+};
+})();
