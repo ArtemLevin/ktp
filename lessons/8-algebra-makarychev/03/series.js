4279d54a7bd384b2fd1d8c2ed143429f4014e4a3
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-04 15:25:36 +0300

    content: scaffold grade 8 equations series

---
+++ lessons/8-algebra-makarychev/03/series.js
@@ -0,0 +1,25 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={
+  meta:{
+    rowId:'8-algebra-makarychev',
+    topicIndex:2,
+    topicId:'8-algebra-makarychev::2',
+    topicNumber:3,
+    grade:8,
+    subject:'Алгебра',
+    book:'Макарычев',
+    topicTitle:'Квадратные уравнения и системы уравнений',
+    schoolYear:'2026/27',
+    plannedWeeks:'11–19',
+    totalLessons:27,
+    courseLessonStart:31,
+    courseLessonEnd:57,
+    topicHref:'../../../topics/8-algebra-makarychev/03.html',
+    ktpHref:'../../../index.html?focus=8-algebra-makarychev&view=timeline',
+    catalogHref:'index.html'
+  },
+  corrections:{},
+  lessons:[]
+};
+})();
