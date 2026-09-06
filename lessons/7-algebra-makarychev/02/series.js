ba944e996af58e6a50e6c2057b10dabaac7aab99
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-06 10:39:03 +0300

    fix: add grade 7 course lesson ranges

--- lessons/7-algebra-makarychev/02/series.js
+++ lessons/7-algebra-makarychev/02/series.js
@@ -13,6 +13,9 @@ window.KTP_LESSON_SERIES={
     schoolYear:'2026/27',
     plannedWeeks:'7–10',
     totalLessons:11,
+    courseLessonStart:19,
+    courseLessonEnd:29,
+    courseTotal:102,
     topicHref:'../../../topics/7-algebra-makarychev/02.html',
     ktpHref:'../../../index.html?focus=7-algebra-makarychev&view=timeline',
     catalogHref:'index.html'
