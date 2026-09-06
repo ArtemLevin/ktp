dfe8bd6c875ff1c8fcd2a63718b2d0df64741f4c
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-06 17:44:06 +0300

    Add grade 5 topic 05 lesson series metadata

---
+++ lessons/5-math-vilenkin/05/series.js
@@ -0,0 +1,7 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={
+ meta:{rowId:'5-math-vilenkin',topicIndex:4,topicId:'5-math-vilenkin::4',topicNumber:5,grade:5,subject:'Математика',book:'Виленкин',topicTitle:'Площадь и объём',schoolYear:'2026/27',plannedWeeks:'13–15',totalLessons:10,courseLessonStart:64,courseLessonEnd:73,courseTotal:170,topicHref:'../../../topics/5-math-vilenkin/05.html',ktpHref:'../../../index.html?focus=5-math-vilenkin&view=timeline',catalogHref:'index.html'},
+ lessons:[],corrections:{}
+};
+})();
