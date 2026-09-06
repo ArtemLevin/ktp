f15d319c7cb4eff0aacb2993c0ef9714251bc4b4
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-06 12:25:04 +0300

    Add grade 5 topic 01 lesson series metadata

---
+++ lessons/5-math-vilenkin/01/series.js
@@ -0,0 +1,26 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={
+  meta:{
+    rowId:'5-math-vilenkin',
+    topicIndex:0,
+    topicId:'5-math-vilenkin::0',
+    topicNumber:1,
+    grade:5,
+    subject:'Математика',
+    book:'Виленкин',
+    topicTitle:'Натуральные числа',
+    schoolYear:'2026/27',
+    plannedWeeks:'1–4',
+    totalLessons:18,
+    courseLessonStart:1,
+    courseLessonEnd:18,
+    courseTotal:170,
+    topicHref:'../../../topics/5-math-vilenkin/01.html',
+    ktpHref:'../../../index.html?focus=5-math-vilenkin&view=timeline',
+    catalogHref:'index.html'
+  },
+  corrections:{},
+  lessons:[]
+};
+})();
