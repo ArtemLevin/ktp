cecdfc467ee336a194ca40833acb64c8a5a92365
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-06 18:49:58 +0300

    Implement grade 5 Vilenkin topic 07
    * Add grade 5 topic 07 content
    
    * Add grade 5 topic 07 series metadata
    
    * Add grade 5 topic 07 lesson data
    
    * Add grade 5 topic 07 lesson pages
    
    * Add grade 5 topic 07 thematic assessments
    
    * Add grade 5 topic 07 independent assessment page
    
    * Add grade 5 topic 07 control assessment page
    
    * Publish grade 5 topic 07 lesson link
    
    * Publish grade 5 topic 07 assessment links
    
    * Add grade 5 topic 07 QA gate
    
    * Add grade 5 topic 07 QA workflow
    
    * Fix grade 5 topic 07 challenge answer
    
    * Fix topic 07 QA comparison assertion
    
    * Sync grade 5 topic 07 project status
    
    * Sync grade 5 topic 07 lesson catalog
    
    * Sync grade 5 topic 07 assessment catalog

---
+++ lessons/5-math-vilenkin/07/series.js
@@ -0,0 +1,4 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={meta:{rowId:'5-math-vilenkin',topicIndex:6,topicId:'5-math-vilenkin::6',topicNumber:7,grade:5,subject:'Математика',book:'Виленкин',topicTitle:'Сложение и вычитание дробей с одинаковыми знаменателями',schoolYear:'2026/27',plannedWeeks:'18–20',totalLessons:10,courseLessonStart:89,courseLessonEnd:98,courseTotal:170,topicHref:'../../../topics/5-math-vilenkin/07.html',ktpHref:'../../../index.html?focus=5-math-vilenkin&view=timeline',catalogHref:'index.html'},lessons:[],corrections:{}};
+})();
