6f0ed5d484fa926a2651be4d11e9f587db7970bc
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-07 16:33:54 +0300

    Add grade 5 topic 11 series metadata

---
+++ lessons/5-math-vilenkin/11/series.js
@@ -0,0 +1,4 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={meta:{rowId:'5-math-vilenkin',topicIndex:10,topicId:'5-math-vilenkin::10',topicNumber:11,grade:5,subject:'Математика',book:'Виленкин',topicTitle:'Умножение и деление десятичных дробей',schoolYear:'2026/27',plannedWeeks:'27–31',totalLessons:20,courseLessonStart:134,courseLessonEnd:153,courseTotal:170,globalStart:134,topicHref:'../../../topics/5-math-vilenkin/11.html',ktpHref:'../../../index.html?focus=5-math-vilenkin&view=timeline',catalogHref:'index.html'},lessons:[],corrections:{}};
+})();
