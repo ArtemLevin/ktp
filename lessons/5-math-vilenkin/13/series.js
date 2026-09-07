107cde619a181b8177b717bdb22816053db488ca
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-07 17:18:16 +0300

    Add grade 5 topic 13 lesson series

---
+++ lessons/5-math-vilenkin/13/series.js
@@ -0,0 +1,4 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={meta:{rowId:'5-math-vilenkin',topicIndex:12,topicId:'5-math-vilenkin::12',topicNumber:13,grade:5,subject:'Математика',book:'Виленкин',topicTitle:'Резерв и повторение',schoolYear:'2026/27',plannedWeeks:'33–34',totalLessons:10,courseLessonStart:161,courseLessonEnd:170,courseTotal:170,globalStart:161,topicHref:'../../../topics/5-math-vilenkin/13.html',ktpHref:'../../../index.html?focus=5-math-vilenkin&view=timeline',catalogHref:'index.html'},lessons:[],corrections:{}};
+})();
