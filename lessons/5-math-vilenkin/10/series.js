794feeb1344197e3bc55ac57dbec3179d9ea0632
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-07 15:40:52 +0300

    Add grade 5 topic 10 series metadata

---
+++ lessons/5-math-vilenkin/10/series.js
@@ -0,0 +1,4 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={meta:{rowId:'5-math-vilenkin',topicIndex:9,topicId:'5-math-vilenkin::9',topicNumber:10,grade:5,subject:'Математика',book:'Виленкин',topicTitle:'Десятичные дроби',schoolYear:'2026/27',plannedWeeks:'24–27',totalLessons:15,courseLessonStart:119,courseLessonEnd:133,courseTotal:170,globalStart:119,topicHref:'../../../topics/5-math-vilenkin/10.html',ktpHref:'../../../index.html?focus=5-math-vilenkin&view=timeline',catalogHref:'index.html'},lessons:[],corrections:{}};
+})();
