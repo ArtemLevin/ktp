fee5fdeb4dbb3a84e331ec0e7c92819d961c6bfa
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-07 17:06:37 +0300

    Implement grade 5 Vilenkin topic 12 Protractor
    Add the full protractor topic for lessons 154–160, thematic and lesson-level assessments, an interactive protractor laboratory, QA, navigation links, and synchronized documentation.

---
+++ lessons/5-math-vilenkin/12/series.js
@@ -0,0 +1,4 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={meta:{rowId:'5-math-vilenkin',topicIndex:11,topicId:'5-math-vilenkin::11',topicNumber:12,grade:5,subject:'Математика',book:'Виленкин',topicTitle:'Транспортир',schoolYear:'2026/27',plannedWeeks:'31–32',totalLessons:7,courseLessonStart:154,courseLessonEnd:160,courseTotal:170,globalStart:154,topicHref:'../../../topics/5-math-vilenkin/12.html',ktpHref:'../../../index.html?focus=5-math-vilenkin&view=timeline',catalogHref:'index.html'},lessons:[],corrections:{}};
+})();
