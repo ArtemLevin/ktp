d5d960985de02a9127c1345f6fe07c09883829a0
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-11 19:29:33 +0300

    assessment: add grade 8 geometry topic 03 thematic assessment scaffold

---
+++ assessments/8-geometry-atanasyan/03/28-44.js
@@ -0,0 +1,17 @@
+(function(){
+'use strict';
+window.KTP_ASSESSMENTS = window.KTP_ASSESSMENTS || {};
+window.KTP_ASSESSMENTS['8-geometry-atanasyan::03'] = {
+ title:'Подобные треугольники',
+ selfCheck:{variants:6,tasks:7,maxScore:14},
+ control:{variants:6,tasks:10,maxScore:20},
+ sections:[
+  'Пропорциональные отрезки',
+  'Подобные треугольники',
+  'Признаки подобия',
+  'Применение подобия',
+  'Прямоугольный треугольник'
+ ],
+ note:'Полный комплект вариантов заполняется отдельным assessment-layer с проверяемыми ответами.'
+};
+})();
