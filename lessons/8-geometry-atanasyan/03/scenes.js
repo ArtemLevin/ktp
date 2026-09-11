f959ed343c41abc2d4196a747c4dea3b3b564419
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-11 17:24:31 +0300

    content: add similarity topic 03 lesson scene placeholders

---
+++ lessons/8-geometry-atanasyan/03/scenes.js
@@ -0,0 +1,9 @@
+(function(){
+'use strict';
+window.KTP_SCENES=window.KTP_SCENES||{};
+window.KTP_SCENES['8-geometry-atanasyan::03']={
+28:{type:'segments',title:'Отношение отрезков'},
+29:{type:'segments',title:'Пропорциональные отрезки'},
+30:{type:'triangle',title:'Модель пропорциональных отрезков'}
+};
+})();
