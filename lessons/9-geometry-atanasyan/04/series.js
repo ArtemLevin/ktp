85abb1dcfd8014ebf9ece5ff6a0a3d236842a71b
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-24 01:58:14 +0300

    content: add topic 04 lesson series metadata

---
+++ lessons/9-geometry-atanasyan/04/series.js
@@ -0,0 +1,4 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={meta:{rowId:'9-geometry-atanasyan',topicIndex:3,topicId:'9-geometry-atanasyan::3',topicNumber:4,grade:9,subject:'Геометрия',book:'Атанасян · Бутузов · Кадомцев и др. + ФРП-2025',topicTitle:'Длина окружности и площадь круга',schoolYear:'2026/27',plannedWeeks:'17–21',totalLessons:10,courseLessonStart:33,courseLessonEnd:42,courseTotal:68,topicHref:'../../../topics/9-geometry-atanasyan/04.html',ktpHref:'../../../index.html?focus=9-geometry-atanasyan&view=timeline',catalogHref:'index.html'},lessons:[],geometryScenes:{},corrections:{}};
+})();
