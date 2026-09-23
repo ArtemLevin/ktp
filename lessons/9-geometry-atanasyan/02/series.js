16d1805b3468d09505993b0b7ef86e1db83c72c7
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-24 00:43:28 +0300

    content: add coordinate method lesson series metadata

---
+++ lessons/9-geometry-atanasyan/02/series.js
@@ -0,0 +1,4 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={meta:{rowId:'9-geometry-atanasyan',topicIndex:1,topicId:'9-geometry-atanasyan::1',topicNumber:2,grade:9,subject:'Геометрия',book:'Атанасян · Бутузов · Кадомцев и др. + ФРП-2025',topicTitle:'Метод координат',schoolYear:'2026/27',plannedWeeks:'6–9',totalLessons:8,courseLessonStart:11,courseLessonEnd:18,courseTotal:68,topicHref:'../../../topics/9-geometry-atanasyan/02.html',ktpHref:'../../../index.html?focus=9-geometry-atanasyan&view=timeline',catalogHref:'index.html'},lessons:[],geometryScenes:{},corrections:{}};
+})();
