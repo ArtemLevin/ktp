8a3fd9762642af3d0aa7bdb74187bb46e03e1d54
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-24 10:02:02 +0300

    content: add topic 05 lesson series metadata

---
+++ lessons/9-geometry-atanasyan/05/series.js
@@ -0,0 +1,4 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={meta:{rowId:'9-geometry-atanasyan',topicIndex:4,topicId:'9-geometry-atanasyan::4',topicNumber:5,grade:9,subject:'Геометрия',book:'Атанасян · Бутузов · Кадомцев и др. + ФРП-2025',topicTitle:'Движения',schoolYear:'2026/27',plannedWeeks:'22–26',totalLessons:9,courseLessonStart:43,courseLessonEnd:51,courseTotal:68,topicHref:'../../../topics/9-geometry-atanasyan/05.html',ktpHref:'../../../index.html?focus=9-geometry-atanasyan&view=timeline',catalogHref:'index.html'},lessons:[],geometryScenes:{},corrections:{}};
+})();
