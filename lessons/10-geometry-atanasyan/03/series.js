bb0f8ec944ced90d7c3f11cf8fdb27200cf466d6
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-24 12:48:25 +0300

    content: scaffold grade 10 geometry topic 03 lessons

---
+++ lessons/10-geometry-atanasyan/03/series.js
@@ -0,0 +1,15 @@
+(function(){
+'use strict';
+window.KTP_LESSON_SERIES={
+ meta:{
+  rowId:'10-geometry-atanasyan',topicIndex:2,topicId:'10-geometry-atanasyan::2',topicNumber:3,
+  grade:10,subject:'Геометрия',book:'Атанасян · Бутузов · Кадомцев и др. + ФРП-2025',
+  topicTitle:'Перпендикулярность прямых и плоскостей',
+  schoolYear:'2026/27',plannedWeeks:'13–22',totalLessons:19,courseLessonStart:24,courseLessonEnd:42,courseTotal:68,
+  topicHref:'../../../topics/10-geometry-atanasyan/03.html',
+  ktpHref:'../../../index.html?focus=10-geometry-atanasyan&view=timeline',
+  catalogHref:'index.html'
+ },
+ lessons:[],spatialScenes:{},corrections:{}
+};
+})();
